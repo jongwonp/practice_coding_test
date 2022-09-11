7fe5ad00c81fb61f7f522216b8d66a01d67fe090
jongwonp <87924173+jongwonp@users.noreply.github.com>
2022-09-11 11:46:19 +0900

    Update 압축.js

--- Level 2/압축.js
+++ Level 2/압축.js
@@ -1,21 +1,21 @@
 function solution(msg) {
     let answer = [];
 
-    let alphabet = [];
+    let alphabet = [];                              // A-Z가 담긴 배열을 만든다.
     for(let i = 65; i <= 90; i++){
         alphabet.push(String.fromCharCode(i));    
     }
 
-    let map = new Map();
+    let map = new Map();                            // map에 A-Z를 각각 key로, 인덱스+1을 값으로 넣어준다.
     for(const char of alphabet){
         map.set(char,alphabet.indexOf(char) + 1)
     }
 
-    let temp = '';
+    let temp = '';                                  // 문자열 msg에서 한 글자씩 저장해볼 변수 선언
     for(let i = 0; i < msg.length + 1; i++){
-        if(map.has(temp + msg[i])){
+        if(map.has(temp + msg[i])){                 // temp에 현재 순번의 한 글자를 합친 글자(msg[i])가 map에 있으면 그대로 temp에 현재 순번의 글자를 합침.
             temp += msg[i]
-        } else {
+        } else {                                    // 그게 아니라면 map에 있는 temp를 answer배열에 넣고, temp + msg[i]를 map에 새롭게 집어 넣음. 마지막으로 temp를 msg[i]로 설정
             answer.push(map.get(temp))
             map.set(temp + msg[i], map.size +1);
             temp = msg[i];
